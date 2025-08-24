a4fa39d3f6bbdb80bf8c10ea0593e62f467f0b03
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2025-08-24 20:51:08 +0200

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,10 +1,9 @@
 "use strict";
 //@ts-nocheck
 Office.onReady((info) => {
-    var _a;
     // Check that we loaded into Word
     if (info.host === Office.HostType.Word) {
-        (_a = document.getElementById("helloButton")) === null || _a === void 0 ? void 0 : _a.onclick = sayHello;
+        //document.getElementById("helloButton")?.onclick = sayHello;
         const btnEditWord = document.getElementById("edit");
         btnEditWord === null || btnEditWord === void 0 ? void 0 : btnEditWord.onclick = () => altert("Edit Word Is Working");
     }
