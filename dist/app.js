d03910ecac9fd8cbd85adcddc00d3d80d1dfe33a
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2025-08-24 21:08:13 +0200

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,22 @@
 "use strict";
-//@ts-nocheck
+//@ts-ignore
 Office.onReady((info) => {
     // Check that we loaded into Word
+    //@ts-ignore
     if (info.host === Office.HostType.Word) {
         //document.getElementById("helloButton")?.onclick = sayHello;
         const btnEditWord = document.getElementById("edit");
-        btnEditWord === null || btnEditWord === void 0 ? void 0 : btnEditWord.onclick = () => alert("Edit Word Is Working");
-        btnEditWord === null || btnEditWord === void 0 ? void 0 : btnEditWord.ondblclick = () => sayHello();
+        //btnEditWord?.onclick = () => alert("Edit Word Is Working");
+        if (btnEditWord)
+            btnEditWord.ondblclick = () => sayHello('Contracts App Works');
     }
 });
-function sayHello() {
+function sayHello(sentence) {
+    //@ts-ignore
     return Word.run((context) => {
         // insert a paragraph at the start of the document.
-        const paragraph = context.document.body.insertParagraph("Contracts App Works", Word.InsertLocation.start);
+        //@ts-ignore
+        const paragraph = context.document.body.insertParagraph(sentence, Word.InsertLocation.start);
         // sync the context to run the previous API call, and return.
         return context.sync();
     });
