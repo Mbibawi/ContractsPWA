aac85615a83a05774eb17f3e49d6d7dfe76c9ae3
Mbibawi <69045020+Mbibawi@users.noreply.github.com>
2025-08-30 20:20:26 +0200

    Add files via upload

--- dist/app.js
+++ dist/app.js
@@ -331,25 +331,27 @@ async function customizeContract() {
         return showNotification('Failed to create the template');
     await selectCtrls();
     async function selectCtrls() {
-        const [keep, url] = await Word.run(async (context) => {
-            const allRT = context.document.contentControls;
-            allRT.load(['title', 'tag', 'contentControls']);
-            await context.sync();
-            const ctrls = allRT.items
-                .filter(ctrl => OPTIONS.includes(ctrl.tag))
-                .entries();
-            const selected = [];
-            //for (const ctrl of ctrls) 
-            //  await promptForSelection(ctrl, selected);
-            const keep = selected.filter(title => !title.startsWith('!'));
+        await Word.run(async (context) => {
             const newDoc = context.application.createDocument(template);
             newDoc.open();
             await context.sync();
-            return [keep, ''];
-            const fileName = promptForInput('Provide the fileName');
-            newDoc.save(Word.SaveBehavior.prompt, fileName);
+            //const fileName = promptForInput('Provide the fileName');
+            //newDoc.save(Word.SaveBehavior.prompt, fileName);
+            (async function myLogic() {
+                return;
+                const allRT = context.document.contentControls;
+                allRT.load(['title', 'tag', 'contentControls']);
+                await context.sync();
+                const ctrls = allRT.items
+                    .filter(ctrl => OPTIONS.includes(ctrl.tag))
+                    .entries();
+                const selected = [];
+                for (const ctrl of ctrls)
+                    await promptForSelection(ctrl, selected);
+                const keep = selected.filter(title => !title.startsWith('!'));
+            })();
         });
-        async function customizeNewDoc() {
+        async function customizeNewDoc(keep) {
             await Word.run(async (context) => {
                 const newURL = getFileURL();
                 //if (newURL !== url) return;
